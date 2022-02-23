f469fea6b81da4f84147114f99da4335426e331e
donellgill <donell.gill@outlook.com>
2022-02-22 23:24:28 -0500

    defined user index action and route, seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(first_name: 'Alex', last_name: 'Bronson', user_name: 'BestBuddy', email: 'xyz123@hotmail.com')
+User.create!(first_name: 'Betty', last_name: 'Charger', user_name: 'seeAllNoAll', email: 'supercharged@gmail.com')
+User.create!(first_name: 'Corey', last_name: 'Datsun', user_name: 'FrencePrish', email: '987abc@aol.com')
+User.create!(first_name: 'Delroy', last_name: 'Eggbert', user_name: 'fastcash44', email: 'fastcash44@gmail.com')

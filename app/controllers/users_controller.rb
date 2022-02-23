6a87c9fac6929fa7783f86857d3e743ae3e82e8b
donellgill <donell.gill@outlook.com>
2022-02-22 23:48:05 -0500

    defined user show action and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
+    # get all users /users
+
     def index
         users = User.all
         render json: users
+    end
+
+    # get a particular user /user/:id
+
+    def show
+            user = User.find(user_params[:id])
+        if user
+            render json: user
+        else
+            render json: {error: "User not found" }, status: :not_found
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:first_name, :last_name, :user_name, :email, :id)
     end
 end
